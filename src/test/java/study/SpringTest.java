43de7459e204de17447217afc5b2a10b80043a61
juntae <juntae6942@o.cnu.ac.kr>
2024-01-08 21:34:39 +0900

    spring study 요구사항 1

---
+++ src/test/java/study/SpringTest.java
@@ -0,0 +1,17 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SpringTest {
+    @Test
+    void test() {
+        //given
+        String str = "1,2";
+        //when
+        String[] result = str.split(",");
+        //then
+        Assertions.assertThat(result)
+                .contains("1","2");
+    }
+}
