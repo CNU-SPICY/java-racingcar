026cc0d86926993e5f36098a2924040f8bf136c3
Sunkist18 <dandalf@o.cnu.ac.kr>
2024-01-10 17:49:54 +0900

    feat: Implement Game flow in RacingCarApplication and Game class
    1. Modified `RacingCarApplication` to initiate the `Game` class and call the `start` method. This serves as the entry point for the racing car game.
    2. Enhanced `Game` class:
       - Added `InputView` and `OutputView` instances for handling input and output operations.
       - Implemented the `start` method to encapsulate the game's workflow, including reading car names and number of trials, executing race logic, and displaying results and winners.

--- src/main/java/racingcar/RacingCarApplication.java
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -1,2 +1,10 @@
-package racingcar;public class RacingCarApplication {
+package racingcar;
+
+import racingcar.controller.Game;
+
+public class RacingCarApplication {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
 }
