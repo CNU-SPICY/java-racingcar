ba9f2553d5f3ebd0679e720c32e56cb9944e19ee
Sunkist18 <dandalf@o.cnu.ac.kr>
2024-01-10 17:47:22 +0900

    feat: Add getWinners method to Cars class in racingcar domain
    Implemented a new method 'getWinners' in the Cars class

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,7 +1,10 @@
 package racingcar.domain;
 
+import java.util.Arrays;
+
 public class Cars {
     private final Car[] cars;
+
     public Cars(String[] carNames) {
         cars = new Car[carNames.length];
         for (int i = 0; i < carNames.length; i++) {
@@ -17,5 +20,15 @@ public class Cars {
 
     public Car[] getCars() {
         return cars;
+    }
+
+    public Car[] getWinners() {
+        int maxDistance = Arrays.stream(cars)
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+        return Arrays.stream(cars)
+                .filter(car -> car.getPosition() == maxDistance)
+                .toArray(Car[]::new);
     }
 }
