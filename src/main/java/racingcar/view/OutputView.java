95a24058c683d7949a1743f78f424b59d7ea78c3
Sunkist18 <dandalf@o.cnu.ac.kr>
2024-01-10 17:18:38 +0900

    feat: Implement constant messages and update OutputView for user prompts
    - Added `ConstantMessage` enum to manage constant user prompt messages.
    - Implemented `printInputCarNames` and `printInputNumberOfTrials` methods in `OutputView` to display relevant prompts using constant messages.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,17 @@
-package racingcar.view;public class OutputView {
+package racingcar.view;
+
+import racingcar.view.constants.ConstantMessage;
+
+public class OutputView {
+
+    public OutputView() {
+    }
+
+    public void printInputCarNames() {
+        System.out.println(ConstantMessage.INPUT_CAR_NAMES.getMessage());
+    }
+
+    public void printInputNumberOfTrials() {
+        System.out.println(ConstantMessage.INPUT_NUMBER_OF_TRIALS.getMessage());
+    }
 }
---
+++ src/main/java/racingcar/view/constants/ConstantMessage.java
@@ -0,0 +1,16 @@
+package racingcar.view.constants;
+
+public enum ConstantMessage {
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)."),
+    INPUT_NUMBER_OF_TRIALS("시도할 회수는 몇회인가요?");
+
+    private final String message;
+
+    ConstantMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
