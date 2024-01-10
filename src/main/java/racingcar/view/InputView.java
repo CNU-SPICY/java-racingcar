0ae8a63793f07a770181fa63e4420adc7d2fe86e
Sunkist18 <dandalf@o.cnu.ac.kr>
2024-01-10 17:09:41 +0900

    feat: Enhance InputView with user input handling for car names
    - Integrated `InputValidator` into `InputView` to validate car names.
    - Added `getInput` method for reading user input using `Scanner`.
    - Implemented `inputCarNames` method to receive and validate car names input from the user.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,19 @@
 package racingcar.view;
 
+import racingcar.validator.InputValidator;
+import java.util.Scanner;
+
 public class InputView {
+    private final InputValidator inputValidator = new InputValidator();
+    private final Scanner scanner = new Scanner(System.in);
+
+    private String getInput() {
+        return scanner.nextLine();
+    }
+
+    public String[] inputCarNames() {
+        String carNames = getInput();
+        inputValidator.validateCarNames(carNames);
+        return carNames.split(",");
+    }
 }
