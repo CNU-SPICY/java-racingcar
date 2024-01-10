8782a0ba69f95ea13cd2db29766c0bf26d3c2612
Sunkist18 <dandalf@o.cnu.ac.kr>
2024-01-10 17:36:02 +0900

    feat: Update ConstantMessage and OutputView for result display in racingcar
    - Expanded `ConstantMessage` enum with 'RESULT' message for results section.
    - Enhanced `OutputView` with `printResult` and `printCars` methods to display race results and car positions.
    - Added `printPosition` method in `OutputView` for visual representation of car positions.

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,6 @@
 package racingcar.view;
 
+import racingcar.domain.Cars;
 import racingcar.view.constants.ConstantMessage;
 
 public class OutputView {
@@ -13,5 +14,24 @@ public class OutputView {
 
     public void printInputNumberOfTrials() {
         System.out.println(ConstantMessage.INPUT_NUMBER_OF_TRIALS.getMessage());
+    }
+
+    public void printResult() {
+        System.out.println(ConstantMessage.RESULT.getMessage());
+    }
+
+    public void printCars(Cars cars) {
+        for (int i = 0; i < cars.getCars().length; i++) {
+            System.out.print(cars.getCars()[i].getCarName() + " : ");
+            printPosition(cars.getCars()[i].getPosition());
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private void printPosition(int position) {
+        for (int i = 0; i < position; i++) {
+            System.out.print("-");
+        }
     }
 }
