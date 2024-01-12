7ed162fd9961b7e09c1e3c838697aa536ff00ed4
hsjni0110 <hansukjinid@gmail.com>
2024-01-12 17:44:26 +0900

    refactor: input view 입력 상수로 리팩터링

--- src/test/java/InputViewTest.java
+++ src/test/java/InputViewTest.java
@@ -3,9 +3,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
-import racingcar.domain.validator.CarNamesValidator;
-import racingcar.view.InputView;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.*;
@@ -16,6 +13,6 @@ public class InputViewTest {
 
     @DisplayName("getCarNames 메서드 테스트")
     @Test
-    void getCarNamesTest() throws IOException {/
+    void getCarNamesTest() throws IOException {
     }
 }
