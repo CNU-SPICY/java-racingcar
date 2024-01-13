ec95545c4267324a5cb6e18f830a436e79b39351
sans29374 <sansu7904@gmail.com>
2024-01-13 11:42:54 +0900

    InputView 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,5 +1,30 @@
 package main.java.racingcar.view;
 
-public class InputView {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class InputView {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String[] getCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String inputNames = scanner.nextLine();
+        String[] carNames = inputNames.split(",");
+
+        while (!isValidCarNames(carNames)) {
+            System.out.println("각 자동차 이름은 5자 이하로 입력해주세요.");
+            inputNames = scanner.nextLine();
+            carNames = inputNames.split(",");
+        }
+        return carNames;
+    }
+
+    public static int getTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return scanner.nextInt();
+    }
+
+    private static boolean isValidCarNames(String[] carNames) {
+        return Arrays.stream(carNames).allMatch(name -> name.trim().length() <= 5);
+    }
 }
