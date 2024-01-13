23fcb04f8e13337c9078819bb328698f002649dc
sans29374 <sansu7904@gmail.com>
2024-01-13 12:02:55 +0900

    <Domain> 현재 상태 처리 <OutputView> 현재 상태 출력

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -20,4 +20,21 @@ public class RacingGame {
             cars.add(new Car(name));
         }
     }
+
+    public void race() {
+        IntStream.range(0, tryCount)
+                .forEach(i -> {
+                    cars.forEach(Car::move);
+                    OutputView.printCurrentStatus(generateStatus()); // 각 회차별 상태 출력
+                });
+    }
+
+    //현재 상태 반환
+    public List<String> generateStatus() {
+        List<String> carsStatus = new ArrayList<>();
+        for (Car car : cars) {
+            carsStatus.add(car.generateStatus());
+        }
+        return carsStatus;
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,12 @@
 package main.java.racingcar.view;
 
+import java.util.List;
+
 public class OutputView {
+    public static void printCurrentStatus(List<String> carsStatus) {
+        for (String status : carsStatus) {
+            System.out.println(status);
+        }
+        System.out.println();
+    }
 }
