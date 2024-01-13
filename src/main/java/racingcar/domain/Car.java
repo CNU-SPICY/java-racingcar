9c16341265e09f54728916419dee748da8867bbe
sans29374 <sansu7904@gmail.com>
2024-01-13 11:53:08 +0900

    <Domain> Input 값에 들어간 차 객체 이름 및 시도 횟수 저장

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,29 @@
 package main.java.racingcar.domain;
 
 public class Car {
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        int randomValue = (int) (Math.random()*10);
+        if (randomValue >= 4) {
+            distance++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String generateStatus() {
+        return name + " : " + "-".repeat(distance);
+    }
 }
--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,4 +1,23 @@
 package main.java.racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import main.java.racingcar.view.OutputView;
+
 public class RacingGame {
+    private List<Car> cars;
+    private int tryCount;
+
+    public RacingGame(String[] carNames, int tryCount) {
+        this.tryCount = tryCount;
+        initializeCars(carNames);
+    }
+
+    private void initializeCars(String[] carNames) {
+        cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+    }
 }
