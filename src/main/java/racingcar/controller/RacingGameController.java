2ed061c43d22d75f6422ec5e0d8dabc9eb08f685
sans29374 <sansu7904@gmail.com>
2024-01-16 13:48:19 +0900

    RacingGameController.java 수정 : 컨트롤러가 tryCount만큼 경주 수행 후 output을 위한 정보를 전달하도록 처리

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -4,15 +4,22 @@ import main.java.racingcar.domain.RacingGame;
 import main.java.racingcar.view.InputView;
 import main.java.racingcar.view.OutputView;
 
+import java.util.List;
+
 public class RacingGameController {
     public void run() {
         final var carNames = InputView.getCarNames();
         final var tryCount = InputView.getTryCount();
 
         final var racingGame = new RacingGame(carNames, tryCount);
-        racingGame.race();
 
-        OutputView.printCurrentStatus(racingGame.generateStatus());
+        for (int i = 0; i < tryCount; i++) {
+            racingGame.race();
+            List<String> carNamesList = racingGame.getNames();
+            List<Integer> distancesList = racingGame.getDistances();
+            OutputView.printCurrentStatus(carNamesList, distancesList);
+        }
+
         OutputView.printWinners(racingGame.getWinners());
     }
 }
